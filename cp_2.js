b13377cc072c16ce7436e1029af09dfbbeeaa511
Cerbando <cmondragonperez@usf.edu>
2026-06-28 22:54:39 -0400

    Add a reusable error function

--- cp_2.js
+++ cp_2.js
@@ -51,3 +51,65 @@ function displayProducts(products) {
         container.appendChild(card);
     });
 }
+// Step 6
+function handleError(error) {
+    console.log(`An error occurred: ${error.message}`);
+}
+const apiURL = "https://www.course-api.com/javascript-store-products";
+
+// Step 3
+function fetchProductsThen() {
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(products => {
+            products.forEach(product => {
+                console.log(product.fields.name);
+            });
+        })
+        .catch(error => {
+            handleError(error);
+        });
+}
+
+// Step 4
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch(apiURL);
+        const products = await response.json();
+
+        displayProducts(products);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+// Step 5
+function displayProducts(products) {
+
+    const container = document.getElementById("product-container");
+
+    container.innerHTML = "";
+
+    products.slice(0, 5).forEach(product => {
+
+        const card = document.createElement("div");
+        card.className = "product-card";
+
+        const name = product.fields.name;
+        const price = product.fields.price / 100;
+        const image = product.fields.image[0].url;
+
+        card.innerHTML = `
+            <img src="${image}" alt="${name}">
+            <h3>${name}</h3>
+            <p>$${price}</p>
+        `;
+
+        container.appendChild(card);
+    });
+}
+
+// Step 6
+function handleError(error) {
+    console.log(`An error occurred: ${error.message}`);
+}
